34779c6cc27a33374e3a65401ce65b4aa944d7ab
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-29 23:45:01 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,9 +27,14 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
