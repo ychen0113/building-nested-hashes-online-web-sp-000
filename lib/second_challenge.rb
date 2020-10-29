953ce3a81f25983ea193587db287fab953b81c0b
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-29 22:01:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,7 +7,12 @@ def second_challenge
       :hero_friends
       }, 
     :capulet => {
-      #your key/value pairs here!
+      :patriarch,
+      :matriarch,
+      :heroine,
+      :heroine_friends
+
+
       }
     
   }
